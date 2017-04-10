1c32206db681e5e1e49173f1d728338501577491
Davidson Sousa <davidson.sousa@gmail.com>
2017-04-10 23:00:25 +0200

    Adding comments for clarity

--- CMSEngine/Data/AccessLayer/IRepository.cs
+++ CMSEngine/Data/AccessLayer/IRepository.cs
@@ -7,12 +7,49 @@ namespace CMSEngine.Data.AccessLayer
 {
     public interface IRepository<T> : IDisposable where T : class
     {
+        /// <summary>
+        /// Gets items based on condition
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         IQueryable<T> Get(Expression<Func<T, bool>> filter = null);
+
+        /// <summary>
+        /// Gets items based on condition for read-only
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         IEnumerable<T> GetReadOnly(Expression<Func<T, bool>> filter = null);
+
+        /// <summary>
+        /// Inserts a record
+        /// </summary>
+        /// <param name="entity"></param>
         void Insert(T entity);
+
+        /// <summary>
+        /// Inserts multiple records in the database
+        /// </summary>
+        /// <param name="entities"></param>
         void InsertBulk(IEnumerable<T> entities);
+
+        /// <summary>
+        /// Updates record
+        /// </summary>
+        /// <param name="entity"></param>
         void Update(T entity);
+
+        /// <summary>
+        /// Updates multiple records based on condition
+        /// </summary>
+        /// <param name="where"></param>
+        /// <param name="update"></param>
         void BulkUpdate(Expression<Func<T, bool>> where, Expression<Func<T, T>> update);
+
+        /// <summary>
+        /// Deletes record from database
+        /// </summary>
+        /// <param name="entity"></param>
         void Delete(T entity);
     }
 }
--- CMSEngine/Data/AccessLayer/IUnitOfWork.cs
+++ CMSEngine/Data/AccessLayer/IUnitOfWork.cs
@@ -4,7 +4,16 @@ namespace CMSEngine.Data.AccessLayer
 {
     public interface IUnitOfWork : IDisposable
     {
+        /// <summary>
+        /// Gets a repository according to the type
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
         IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+        /// <summary>
+        /// Saves all pending changes into the database
+        /// </summary>
         void Save();
     }
 }
