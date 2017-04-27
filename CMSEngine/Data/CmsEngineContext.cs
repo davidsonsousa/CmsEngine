eaf0258de1fdf507784e0379fb9effe759af9b8b
Davidson Sousa <davidson.sousa@gmail.com>
2017-04-27 23:10:07 +0200

    Adding Context and Initializer

--- CMSEngine/Data/CmsEngineContext.cs
+++ CMSEngine/Data/CmsEngineContext.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CmsEngine.Data.Models;
+using CmsEngine.Data.Models.Configuration;
 
 namespace CmsEngine.Data
 {
     [DbConfigurationType(typeof(DbConfig))]
-    public class CmsEngineContext
+    public class CmsEngineContext : DbContext, IDbContext
     {
-        
+        public CmsEngineContext(string connectionString) : base(connectionString)
+        {
+            Database.SetInitializer(new CmsEngineInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            // Forces the database to use the type datetime2
+            modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2").HasPrecision(0));
+
+            // Model configuration
+            modelBuilder.Configurations.Add(new WebsiteConfiguration());
+            modelBuilder.Configurations.Add(new PageConfiguration());
+            modelBuilder.Configurations.Add(new PostConfiguration());
+            modelBuilder.Configurations.Add(new TagConfiguration());
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            var saveTime = DateTime.Now;
+
+            var entries = ChangeTracker.Entries().Where(e => e.Entity is BaseModel && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            var currentUsername = "";//HttpContext.Current?.User?.Identity?.Name;
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DateCreated").CurrentValue = saveTime;
+                    entry.Property("UserCreated").CurrentValue = currentUsername;
+                }
+
+                entry.Property("DateModified").CurrentValue = saveTime;
+                entry.Property("UserModified").CurrentValue = currentUsername;
+            }
+
+            return base.SaveChanges();
+        }
+
+        public DbSet<Website> Websites { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ CMSEngine/Data/CmsEngineInitializer.cs
@@ -0,0 +1,27 @@
+﻿using CmsEngine.Data.Models;
+using System.Data.Entity;
+
+namespace CmsEngine.Data
+{
+    public class CmsEngineInitializer : CreateDatabaseIfNotExists<CmsEngineContext>
+    {
+        protected override void Seed(CmsEngineContext context)
+        {
+            // Create default website
+
+            var website = new Website
+            {
+                Name = "Default Website",
+                Description = "Welcome to the Default Website",
+                Culture = "en-US",
+                SiteUrl = "cmsengine-dev.com"
+            };
+
+            context.Websites.Add(website);
+
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
