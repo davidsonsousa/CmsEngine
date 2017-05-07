d33886dbbcb1355e3f478635f88eb9a6cba2b4d1
Davidson Sousa <dev@davidsonsousa.net>
2017-05-07 16:27:51 +0200

    Refactoring

--- CMSEngine.Api/Controllers/WebsiteController.cs
+++ CMSEngine.Api/Controllers/WebsiteController.cs
@@ -1,11 +1,9 @@
+using CmsEngine.Data.AccessLayer;
+using CmsEngine.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using CmsEngine.Data.AccessLayer;
-using CmsEngine.Services;
 
 namespace CmsEngine.Api.Controllers
 {
