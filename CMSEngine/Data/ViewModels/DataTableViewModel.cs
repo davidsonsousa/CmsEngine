55bdd6b04c028ff481204c9b51b81bd223d5ec84
Davidson Sousa <dev@davidsonsousa.net>
2017-05-15 00:55:25 +0200

    Removing DataTableViewModel for now

--- CMSEngine/Data/ViewModels/DataTableViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CmsEngine.Data.ViewModels
-{
-    public class DataTableViewModel<T> where T: IViewModel
-    {
-        public IEnumerable<T> Items { get; set; }
-    }
-}
