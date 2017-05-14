8352cb7313302e869dcd9bc56078a2aa22d3ab2f
Davidson Sousa <dev@davidsonsousa.net>
2017-05-15 00:55:03 +0200

    Check if property has setter

--- CMSEngine/Extensions/ObjectExtensions.cs
+++ CMSEngine/Extensions/ObjectExtensions.cs
@@ -9,7 +9,7 @@ namespace CmsEngine.Extensions
             foreach (var sourceProp in source.GetType().GetProperties())
             {
                 var targetProp = target.GetType().GetProperties().Where(p => p.Name == sourceProp.Name).FirstOrDefault();
-                if (targetProp != null && targetProp.GetType().Name == sourceProp.GetType().Name)
+                if (targetProp != null && targetProp.CanWrite && targetProp.GetSetMethod() != null && targetProp.GetType().Name == sourceProp.GetType().Name)
                 {
                     targetProp.SetValue(target, sourceProp.GetValue(source));
                 }
