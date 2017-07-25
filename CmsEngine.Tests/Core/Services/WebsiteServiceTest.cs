11bf9238e7a232636834a92309acfc4e9d0edc4d
Davidson Sousa <dev@davidsonsousa.net>
2017-07-26 01:17:09 +0200

    Adding unit tests for Website

---
+++ CmsEngine.Tests/Core/Services/WebsiteServiceTest.cs
@@ -0,0 +1,201 @@
+﻿using AutoMapper;
+using CmsEngine.Data.EditModels;
+using CmsEngine.Data.Models;
+using CmsEngine.Data.ViewModels;
+using CmsEngine.Services;
+using CmsEngine.Tests.Core.Setup;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CmsEngine.Tests.Core.Services
+{
+    public class WebsiteServiceTest
+    {
+        private WebsiteService moqWebsiteService;
+
+        public WebsiteServiceTest()
+        {
+            moqWebsiteService = WebsiteSetup.SetupService();
+        }
+
+        #region Get
+
+        [Fact]
+        public void GetAll_ShouldReturnAllWebsitesAsQueryable()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().Count;
+
+            // Act
+            var response = moqWebsiteService.GetAll();
+
+            // Assert
+            Assert.True(response is IQueryable<Website>, "Response is not IQueryable<Website>");
+            Assert.Equal(expectedResult, response.Count());
+        }
+
+        [Fact]
+        public void GetAllReadOnly_ShouldReturnAllWebsitesAsEnumerable()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().Count;
+
+            // Act
+            var response = moqWebsiteService.GetAllReadOnly();
+
+            // Assert
+            Assert.True(response is IEnumerable<Website>, "Response is not IEnumerable<Website>");
+            Assert.Equal(expectedResult, response.Count());
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnCorrectWebsite()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().FirstOrDefault(q => q.Id == 1).Name;
+
+            // Act
+            var response = moqWebsiteService.GetById(1);
+
+            // Assert
+            Assert.Equal(expectedResult, response.Name);
+        }
+
+        [Fact]
+        public void GetByVanityId_ShouldReturnCorrectWebsite()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().FirstOrDefault(q => q.VanityId == new Guid("8633a850-128f-4425-a2ec-30e23826b7ff")).Name;
+
+            // Act
+            var response = moqWebsiteService.GetById(new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"));
+
+            // Assert
+            Assert.Equal(expectedResult, response.Name);
+        }
+
+        #endregion
+
+        #region Setup
+
+        [Fact]
+        public void SetupEditModel_ShouldReturnNewWebsite()
+        {
+            // Arrange
+
+            // Act
+            var response = moqWebsiteService.SetupEditModel();
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.True(((WebsiteEditModel)response).IsNew, "Item is not new");
+        }
+
+        [Fact]
+        public void SetupEditModel_ById_ShouldReturnCorrectWebsite()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().FirstOrDefault(q => q.Id == 2).Name;
+
+            // Act
+            var response = moqWebsiteService.SetupEditModel(2);
+
+            // Assert
+            Assert.IsType(typeof(WebsiteEditModel), response);
+            Assert.Equal(expectedResult, ((WebsiteEditModel)response).Name);
+        }
+
+        [Fact]
+        public void SetupEditModel_ByVanityId_ShouldReturnCorrectWebsite()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().FirstOrDefault(q => q.VanityId == new Guid("8633a850-128f-4425-a2ec-30e23826b7ff")).Name;
+
+            // Act
+            var response = moqWebsiteService.SetupEditModel(new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"));
+
+            // Assert
+            Assert.IsType(typeof(WebsiteEditModel), response);
+            Assert.Equal(expectedResult, ((WebsiteEditModel)response).Name);
+        }
+
+        [Fact]
+        public void SetupViewModel_ById_ShouldReturnCorrectWebsite()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().FirstOrDefault(q => q.Id == 2).Name;
+
+            // Act
+            var response = moqWebsiteService.SetupViewModel(2);
+
+            // Assert
+            Assert.IsType(typeof(WebsiteViewModel), response);
+            Assert.Equal(expectedResult, ((WebsiteViewModel)response).Name);
+        }
+
+        [Fact]
+        public void SetupViewModel_ByVanityId_ShouldReturnCorrectWebsite()
+        {
+            // Arrange
+            var expectedResult = WebsiteSetup.GetTestWebsites().FirstOrDefault(q => q.VanityId == new Guid("8633a850-128f-4425-a2ec-30e23826b7ff")).Name;
+
+            // Act
+            var response = moqWebsiteService.SetupViewModel(new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"));
+
+            // Assert
+            Assert.IsType(typeof(WebsiteViewModel), response);
+            Assert.Equal(expectedResult, ((WebsiteViewModel)response).Name);
+        }
+
+        #endregion
+
+        #region DB Changes
+
+        [Fact]
+        public void Save_Website()
+        {
+            // Arrange
+
+            // Act
+            var websiteEditModel = new WebsiteEditModel
+            {
+                Name = "Website3",
+                Culture = "cs-cz"
+            };
+
+            var response = moqWebsiteService.Save(websiteEditModel);
+
+            // Assert
+            Assert.False(response.IsError, "Exception thrown");
+        }
+
+        [Fact]
+        public void Delete_Website_By_Id()
+        {
+            // Arrange
+
+            // Act
+            var response = moqWebsiteService.Delete(1);
+
+            // Assert
+            Assert.False(response.IsError, "Exception thrown");
+        }
+
+        [Fact]
+        public void Delete_Website_By_VanityId()
+        {
+            // Arrange
+
+            // Act
+            var response = moqWebsiteService.Delete(new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"));
+
+            // Assert
+            Assert.False(response.IsError, "Exception thrown");
+        }
+
+        #endregion
+    }
+}
---
+++ CmsEngine.Tests/Core/Setup/WebsiteSetup.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using CmsEngine.Data.AccessLayer;
+using CmsEngine.Data.EditModels;
+using CmsEngine.Data.Models;
+using CmsEngine.Data.ViewModels;
+using CmsEngine.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CmsEngine.Tests.Core.Setup
+{
+    public static class WebsiteSetup
+    {
+        /// <summary>
+        /// Returns a list of websites
+        /// </summary>
+        public static List<Website> GetTestWebsites()
+        {
+            return new List<Website>
+                {
+                    new Website { Id = 1, VanityId = new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89"), Name = "Website1", Culture="en-US", Description="Welcome to website 1", IsDeleted = false },
+                    new Website { Id = 2, VanityId = new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"), Name = "Website2", Culture="pt-BR", Description="Welcome to website 2", IsDeleted = false }
+                };
+        }
+
+        /// <summary>
+        /// Returns the EditModel of Id 2
+        /// </summary>
+        /// <returns></returns>
+        public static WebsiteEditModel GetEditModel()
+        {
+            return new WebsiteEditModel { Id = 2, VanityId = new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"), Name = "Website2", Culture = "pt-BR", Description = "Welcome to website 2" };
+        }
+
+        /// <summary>
+        /// Returns the ViewModel of Id 2
+        /// </summary>
+        /// <returns></returns>
+        public static WebsiteViewModel GetViewModel()
+        {
+            return new WebsiteViewModel { Id = 2, VanityId = new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"), Name = "Website2", Culture = "pt-BR", Description = "Welcome to website 2" };
+        }
+
+        /// <summary>
+        /// Setup the Repository and its returning values
+        /// </summary>
+        /// <returns></returns>
+        public static Mock<IRepository<Website>> SetupWebsiteRepository()
+        {
+            var moqRepository = new Mock<IRepository<Website>>();
+            moqRepository.Setup(x => x.Get(It.IsAny<Expression<Func<Website, bool>>>())).Returns(GetTestWebsites().AsQueryable());
+            moqRepository.Setup(x => x.GetReadOnly(It.IsAny<Expression<Func<Website, bool>>>())).Returns(GetTestWebsites());
+
+            return moqRepository;
+        }
+
+        /// <summary>
+        /// Setup the Unit Of Work
+        /// </summary>
+        /// <param name="moqRepository"></param>
+        /// <returns></returns>
+        public static Mock<IUnitOfWork> SetupUnitOfWork(IMock<IRepository<Website>> moqRepository)
+        {
+            var moqUnitOfWork = new Mock<IUnitOfWork>();
+            moqUnitOfWork.Setup(x => x.GetRepository<Website>()).Returns(moqRepository.Object);
+
+            return moqUnitOfWork;
+        }
+
+        /// <summary>
+        /// Setup WebsiteService
+        /// </summary>
+        /// <param name="moqUnitOfWork"></param>
+        /// <returns></returns>
+        public static WebsiteService SetupService()
+        {
+            var moqRepository = SetupWebsiteRepository();
+            var moqUnitOfWork = SetupUnitOfWork(moqRepository);
+            var moqMapper = new Mock<IMapper>();
+            moqMapper.Setup(x => x.Map<Website, WebsiteEditModel>(It.IsAny<Website>())).Returns(GetEditModel());
+            moqMapper.Setup(x => x.Map<Website, WebsiteViewModel>(It.IsAny<Website>())).Returns(GetViewModel());
+
+            return new WebsiteService(moqUnitOfWork.Object, moqMapper.Object);
+        }
+    }
+}
