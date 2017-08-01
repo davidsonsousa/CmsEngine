8e5ed4570b745fdbc9d38abadbfb4ae043a4ad78
Davidson Sousa <dev@davidsonsousa.net>
2017-08-01 23:51:14 +0200

    Adding tests and fixture for tags

---
+++ CmsEngine.Tests/Core/Services/TagServiceTest.cs
@@ -0,0 +1,200 @@
+﻿using CmsEngine.Data.EditModels;
+using CmsEngine.Data.Models;
+using CmsEngine.Data.ViewModels;
+using CmsEngine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using CmsEngine.Tests.Fixtures;
+
+namespace CmsEngine.Test.Core.Services
+{
+    public class TagServiceTest : IClassFixture<TagFixture>
+    {
+        private TagFixture tagFixture;
+        private TagService moqTagService;
+
+        public TagServiceTest(TagFixture fixture)
+        {
+            tagFixture = fixture;
+            moqTagService = tagFixture.Service;
+        }
+
+        #region Get
+
+        [Fact]
+        public void GetAll_ShouldReturnAllTagsAsQueryable()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().Count;
+
+            // Act
+            var response = moqTagService.GetAll();
+
+            // Assert
+            Assert.True(response is IQueryable<Tag>, "Response is not IQueryable<Tag>");
+            Assert.Equal(expectedResult, response.Count());
+        }
+
+        [Fact]
+        public void GetAllReadOnly_ShouldReturnAllTagsAsEnumerable()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().Count;
+
+            // Act
+            var response = moqTagService.GetAllReadOnly();
+
+            // Assert
+            Assert.True(response is IEnumerable<Tag>, "Response is not IEnumerable<Tag>");
+            Assert.Equal(expectedResult, response.Count());
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnCorrectTag()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().FirstOrDefault(q => q.Id == 1).Name;
+
+            // Act
+            var response = moqTagService.GetById(1);
+
+            // Assert
+            Assert.Equal(expectedResult, response.Name);
+        }
+
+        [Fact]
+        public void GetByVanityId_ShouldReturnCorrectTag()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().FirstOrDefault(q => q.VanityId == new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89")).Name;
+
+            // Act
+            var response = moqTagService.GetById(new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89"));
+
+            // Assert
+            Assert.Equal(expectedResult, response.Name);
+        }
+
+        #endregion
+
+        #region Setup
+
+        [Fact]
+        public void SetupEditModel_ShouldReturnNewTag()
+        {
+            // Arrange
+
+            // Act
+            var response = moqTagService.SetupEditModel();
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.True(((TagEditModel)response).IsNew, "Item is not new");
+        }
+
+        [Fact]
+        public void SetupEditModel_ById_ShouldReturnCorrectTag()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().FirstOrDefault(q => q.Id == 2).Name;
+
+            // Act
+            var response = moqTagService.SetupEditModel(2);
+
+            // Assert
+            Assert.IsType(typeof(TagEditModel), response);
+            Assert.Equal(expectedResult, ((TagEditModel)response).Name);
+        }
+
+        [Fact]
+        public void SetupEditModel_ByVanityId_ShouldReturnCorrectTag()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().FirstOrDefault(q => q.VanityId == new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89")).Name;
+
+            // Act
+            var response = moqTagService.SetupEditModel(new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89"));
+
+            // Assert
+            Assert.IsType(typeof(TagEditModel), response);
+            Assert.Equal(expectedResult, ((TagEditModel)response).Name);
+        }
+
+        [Fact]
+        public void SetupViewModel_ById_ShouldReturnCorrectTag()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().FirstOrDefault(q => q.Id == 2).Name;
+
+            // Act
+            var response = moqTagService.SetupViewModel(2);
+
+            // Assert
+            Assert.IsType(typeof(TagViewModel), response);
+            Assert.Equal(expectedResult, ((TagViewModel)response).Name);
+        }
+
+        [Fact]
+        public void SetupViewModel_ByVanityId_ShouldReturnCorrectTag()
+        {
+            // Arrange
+            var expectedResult = tagFixture.GetTestTags().FirstOrDefault(q => q.VanityId == new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89")).Name;
+
+            // Act
+            var response = moqTagService.SetupViewModel(new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89"));
+
+            // Assert
+            Assert.IsType(typeof(TagViewModel), response);
+            Assert.Equal(expectedResult, ((TagViewModel)response).Name);
+        }
+
+        #endregion
+
+        #region DB Changes
+
+        [Fact]
+        public void Save_Tag()
+        {
+            // Arrange
+
+            // Act
+            var tagEditModel = new TagEditModel
+            {
+                Name = "Tag3"
+            };
+
+            var response = moqTagService.Save(tagEditModel);
+
+            // Assert
+            Assert.False(response.IsError, "Exception thrown");
+        }
+
+        [Fact]
+        public void Delete_Tag_By_Id()
+        {
+            // Arrange
+
+            // Act
+            var response = moqTagService.Delete(1);
+
+            // Assert
+            Assert.False(response.IsError, "Exception thrown");
+        }
+
+        [Fact]
+        public void Delete_Tag_By_VanityId()
+        {
+            // Arrange
+
+            // Act
+            var response = moqTagService.Delete(new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89"));
+
+            // Assert
+            Assert.False(response.IsError, "Exception thrown");
+        }
+
+        #endregion
+    }
+}
---
+++ CmsEngine.Tests/Fixtures/TagFixture.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using CmsEngine.Data.AccessLayer;
+using CmsEngine.Data.EditModels;
+using CmsEngine.Data.Models;
+using CmsEngine.Data.ViewModels;
+using CmsEngine.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CmsEngine.Tests.Fixtures
+{
+    public class TagFixture
+    {
+        private Mock<IRepository<Tag>> moqRepository;
+        public Mock<IRepository<Tag>> MoqRepository
+        {
+            get { return moqRepository; }
+        }
+
+        private Mock<IUnitOfWork> moqUnitOfWork;
+        public Mock<IUnitOfWork> MoqUnitOfWork
+        {
+            get { return moqUnitOfWork; }
+        }
+
+        private TagService service;
+        public TagService Service
+        {
+            get { return service; }
+        }
+
+        private Mock<IMapper> moqMapper;
+        public Mock<IMapper> MoqMapper
+        {
+            get { return moqMapper; }
+        }
+
+        public TagFixture()
+        {
+            SetupRepository();
+            SetupUnitOfWork();
+            SetupMapper();
+
+            service = new TagService(moqUnitOfWork.Object, moqMapper.Object);
+        }
+
+        /// <summary>
+        /// Returns a list of tags
+        /// </summary>
+        public List<Tag> GetTestTags()
+        {
+            return new List<Tag>
+                {
+                    new Tag { Id = 1, VanityId = new Guid("278c0380-bdd2-45bb-869b-b94659bc2b89"), Name = "Tag1", IsDeleted = false },
+                    new Tag { Id = 2, VanityId = new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"), Name = "Tag2", IsDeleted = false }
+                };
+        }
+
+        /// <summary>
+        /// Returns the EditModel of Id 2
+        /// </summary>
+        /// <returns></returns>
+        public TagEditModel GetEditModel()
+        {
+            return new TagEditModel { Id = 2, VanityId = new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"), Name = "Tag2" };
+        }
+
+        /// <summary>
+        /// Returns the ViewModel of Id 2
+        /// </summary>
+        /// <returns></returns>
+        public TagViewModel GetViewModel()
+        {
+            return new TagViewModel { Id = 2, VanityId = new Guid("8633a850-128f-4425-a2ec-30e23826b7ff"), Name = "Tag2" };
+        }
+
+        /// <summary>
+        /// Setup the Repository instance and its returning values
+        /// </summary>
+        /// <returns></returns>
+        private void SetupRepository()
+        {
+            moqRepository = new Mock<IRepository<Tag>>();
+            moqRepository.Setup(x => x.Get(It.IsAny<Expression<Func<Tag, bool>>>())).Returns(GetTestTags().AsQueryable());
+            moqRepository.Setup(x => x.GetReadOnly(It.IsAny<Expression<Func<Tag, bool>>>())).Returns(GetTestTags());
+        }
+
+        /// <summary>
+        /// Setup the UnitOfWork instance
+        /// </summary>
+        /// <param name="moqRepository"></param>
+        /// <returns></returns>
+        private void SetupUnitOfWork()
+        {
+            moqUnitOfWork = new Mock<IUnitOfWork>();
+            moqUnitOfWork.Setup(x => x.GetRepository<Tag>()).Returns(MoqRepository.Object);
+        }
+
+        /// <summary>
+        /// Setup Mapper instance
+        /// </summary>
+        private void SetupMapper()
+        {
+            moqMapper = new Mock<IMapper>();
+            moqMapper.Setup(x => x.Map<Tag, TagEditModel>(It.IsAny<Tag>())).Returns(GetEditModel());
+            moqMapper.Setup(x => x.Map<Tag, TagViewModel>(It.IsAny<Tag>())).Returns(GetViewModel());
+        }
+    }
+}
