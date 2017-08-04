b6742695540f22041e47576730f385f51c2e0dc6
Davidson Sousa <dev@davidsonsousa.net>
2017-08-05 01:15:34 +0200

    Improving website component

--- CmsEngine.Ui/ClientApp/app/components/cms/website/website.component.ts
+++ CmsEngine.Ui/ClientApp/app/components/cms/website/website.component.ts
@@ -1,8 +1,22 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, AfterViewInit } from '@angular/core';
+import { WebsiteService } from '../../../services/website.service';
 
 @Component({
   selector: 'cms-website',
-  templateUrl: './website.component.html'
+  templateUrl: './website.component.html',
+  providers: [WebsiteService]
 })
-export class WebsiteComponent {
+export class WebsiteComponent implements AfterViewInit {
+  public websites = [];
+  public columns = [];
+
+  constructor(private websiteService: WebsiteService) { }
+
+  ngAfterViewInit(): void {
+    this.websiteService.getWebsites()
+      .subscribe(websites => {
+        this.websites = websites;
+        this.columns = this.websiteService.extractProperties(this.websites[0]);
+      });
+  }
 }
