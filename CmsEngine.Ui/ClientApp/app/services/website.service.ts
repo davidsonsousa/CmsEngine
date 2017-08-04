6012da3ff7670a292efbc31684f1adbd3625bb5d
Davidson Sousa <dev@davidsonsousa.net>
2017-08-05 00:25:21 +0200

    Adding website.service

---
+++ CmsEngine.Ui/ClientApp/app/services/website.service.ts
@@ -0,0 +1,13 @@
+﻿import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+export class WebsiteService {
+  private readonly endpoint = 'api/wesbite';
+
+  constructor(private http: Http) { }
+
+  public getWebsites() {
+    return this.http.get(this.endpoint)
+      .map(res => res.json());
+  }
+}
