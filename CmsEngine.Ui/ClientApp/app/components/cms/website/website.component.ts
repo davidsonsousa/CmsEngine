1ac16eecfd718694a234021dcb12f4823e83c884
Davidson Sousa <dev@davidsonsousa.net>
2017-08-05 16:19:02 +0200

    Improved website component by adding delete functionality

--- CmsEngine.Ui/ClientApp/app/components/cms/website/website.component.ts
+++ CmsEngine.Ui/ClientApp/app/components/cms/website/website.component.ts
@@ -9,14 +9,22 @@ import { WebsiteService } from '../../../services/website.service';
 export class WebsiteComponent implements AfterViewInit {
   public websites = [];
   public columns = [];
+  public vanityId: string;
 
   constructor(private websiteService: WebsiteService) { }
 
-  ngAfterViewInit(): void {
-    this.websiteService.getWebsites()
+  public ngAfterViewInit(): void {
+    this.websiteService.get()
       .subscribe(websites => {
         this.websites = websites;
         this.columns = this.websiteService.extractProperties(this.websites[0]);
+      });
+  }
+
+  public onDeleteWebsite(vanityId: string) {
+    this.websiteService.delete(vanityId)
+      .subscribe(response => {
+        console.log(response);
       });
   }
 }
