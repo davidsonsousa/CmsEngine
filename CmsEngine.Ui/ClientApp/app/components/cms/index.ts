1a7cc8a20af90157b353ffdf1f8c08cc1994dd81
Davidson Sousa <dev@davidsonsousa.net>
2017-08-05 16:17:54 +0200

    Added website edit model

--- CmsEngine.Ui/ClientApp/app/components/cms/index.ts
+++ CmsEngine.Ui/ClientApp/app/components/cms/index.ts
@@ -3,5 +3,6 @@ export * from './page/page.component';
 export * from './post/post.component';
 export * from './tag/tag.component';
 export * from './website/website.component';
+export * from './website/website-edit.component';
 
 export * from './shared/list.component';
