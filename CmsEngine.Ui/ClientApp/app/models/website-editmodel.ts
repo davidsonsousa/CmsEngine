c066d5adde16a0ab3b6203c08743f1789ae39ad3
Davidson Sousa <dev@davidsonsousa.net>
2017-08-05 16:17:44 +0200

    Added website edit model

---
+++ CmsEngine.Ui/ClientApp/app/models/website-editmodel.ts
@@ -0,0 +1,10 @@
+﻿export class WebsiteEditModel {
+  public id: number;
+  public vanityId: string;
+  public name: string;
+  public description: string;
+  public culture: string;
+  public urlFormat: string;
+  public dateFormat: string;
+  public siteUrl: string
+}
