dc8fc9335fea5097f29c639b70f7aef779a5540d
Davidson Sousa <dev@davidsonsousa.net>
2017-08-10 00:37:07 +0200

    Adding tag.service

---
+++ CmsEngine.Ui/ClientApp/app/services/tag.service.ts
@@ -0,0 +1,34 @@
+﻿import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { ToastyService } from 'ng2-toasty';
+
+import { Service } from './service';
+import { TagEditModel } from '../models/index';
+
+@Injectable()
+export class TagService extends Service {
+  constructor(
+    private protocol: Http,
+    private toastySvc: ToastyService,
+    private routing: Router
+  ) {
+    super(protocol, 'api/tag', toastySvc, routing);
+  }
+
+  /**
+   * Create a new tag
+   * @param tagEditModel
+   */
+  public create(tagEditModel: TagEditModel): any {
+    return this.post(tagEditModel);
+  }
+
+  /**
+   * Update a specific tag
+   * @param tagEditModel
+   */
+  public update(tagEditModel: TagEditModel): any {
+    return this.put(tagEditModel.vanityId, tagEditModel);
+  }
+}
