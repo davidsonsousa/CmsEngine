25aaa5258dcbe3b7f9fc0237de9f7c3d2577c593
Davidson Sousa <dev@davidsonsousa.net>
2017-08-12 19:48:35 +0200

    Adding Tag and Category to the mapping profile

--- CmsEngine/Data/MappingProfile.cs
+++ CmsEngine/Data/MappingProfile.cs
@@ -9,7 +9,31 @@ namespace CmsEngine.Data
     {
         public MappingProfile()
         {
+            MapCategory();
+            MapTag();
             MapWebsite();
+        }
+
+        private void MapCategory()
+        {
+            CreateMap<Category, CategoryEditModel>();
+            CreateMap<Category, CategoryViewModel>();
+
+            CreateMap<CategoryEditModel, Category>()
+                .ForMember(em => em.Id, opt => opt.Ignore())
+                .ForMember(em => em.VanityId, opt => opt.Ignore());
+            CreateMap<CategoryViewModel, Category>();
+        }
+
+        private void MapTag()
+        {
+            CreateMap<Tag, TagEditModel>();
+            CreateMap<Tag, TagViewModel>();
+
+            CreateMap<TagEditModel, Tag>()
+                .ForMember(em => em.Id, opt => opt.Ignore())
+                .ForMember(em => em.VanityId, opt => opt.Ignore());
+            CreateMap<TagViewModel, Tag>();
         }
 
         private void MapWebsite()
