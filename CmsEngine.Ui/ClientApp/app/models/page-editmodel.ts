2b706bc62458ab713cb98d99b2533031a990ff5f
Davidson Sousa <dev@davidsonsousa.net>
2017-08-13 23:21:34 +0200

    Adding page-editmodel.ts

---
+++ CmsEngine.Ui/ClientApp/app/models/page-editmodel.ts
@@ -0,0 +1,13 @@
+﻿import { DocumentStatus } from "./enums";
+
+export class PageEditModel {
+  public id: number;
+  public vanityId: string;
+  public title: string;
+  public slug: string;
+  public description: string;
+  public documentContent: string;
+  public author: string;
+  public status: DocumentStatus;
+  public publishedOn: Date
+}
