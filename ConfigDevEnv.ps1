69146a00f3671a4f2dbf0546f9a42e959f8e1f0e
Davidson Sousa <dev@davidsonsousa.net>
2017-08-13 16:57:19 +0200

    Renaming PowerShell file

---
+++ ConfigDevEnv.ps1
@@ -0,0 +1,11 @@
+﻿# Set envitonment to 'Development'
+$Env:ASPNETCORE_ENVIRONMENT = "Development"
+
+Write-Host "`nEnvironment set to " -NoNewline;
+Write-Host "Development`n" -ForegroundColor Green
+
+# Set url to 'cmsengine.dev'
+$env:ASPNETCORE_URLS="http://cmsengine.dev:5000"
+
+Write-Host "Url set to " -NoNewline;
+Write-Host "cmsengine.dev`n" -ForegroundColor Green
