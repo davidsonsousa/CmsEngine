b159bf73014773c209344cd5797392f4a1482fb0
Davidson Sousa <dev@davidsonsousa.net>
2017-08-14 22:51:46 +0200

    Migrating from .NET Core 1.1 to 2.0

--- CmsEngine.Ui/Program.cs
+++ CmsEngine.Ui/Program.cs
@@ -1,24 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace WebApplicationBasic
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+namespace WebApplicationBasic {
+    public class Program {
+        public static void Main(string[] args) {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
         }
     }
 }
