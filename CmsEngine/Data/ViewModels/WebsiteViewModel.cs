ef09f6821ba5e5d2136b0ad1e4000028fb0a26a9
Davidson Sousa <dev@davidsonsousa.net>
2017-10-07 17:14:20 +0200

    Adding attributes for data table

--- CmsEngine/Data/ViewModels/WebsiteViewModel.cs
+++ CmsEngine/Data/ViewModels/WebsiteViewModel.cs
@@ -1,17 +1,25 @@
-﻿namespace CmsEngine.Data.ViewModels
+using CmsEngine.Attributes;
+
+namespace CmsEngine.Data.ViewModels
 {
     public class WebsiteViewModel : BaseViewModel, IViewModel
     {
+        [Searchable, Orderable, ShowOnDataTable(0)]
         public string Name { get; set; }
 
+        [Searchable, Orderable, ShowOnDataTable(1)]
         public string Description { get; set; }
 
+        [Searchable, Orderable, ShowOnDataTable(2)]
         public string Culture { get; set; }
 
+        [Searchable, Orderable, ShowOnDataTable(3)]
         public string UrlFormat { get; set; } = $"{Constants.SiteUrl}/{Constants.Type}/{Constants.Slug}";
 
+        [Searchable, Orderable, ShowOnDataTable(4)]
         public string DateFormat { get; set; } = "MM/dd/yyyy";
 
+        [Searchable, Orderable, ShowOnDataTable(5)]
         public string SiteUrl { get; set; }
     }
 }
