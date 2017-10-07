356a8b7d3318481703be8a75eb658e6ca695971c
Davidson Sousa <dev@davidsonsousa.net>
2017-10-07 17:00:29 +0200

    Adapting list component to the new data table api

--- CmsEngine.Ui/ClientApp/app/shared/list.component.ts
+++ CmsEngine.Ui/ClientApp/app/shared/list.component.ts
@@ -1,29 +1,38 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DataTableViewModel } from '../models/index';
 
 @Component({
   selector: 'cms-list',
   templateUrl: './list.component.html'
 })
 export class ListComponent {
-  @Input() tableColumns = [];
-  @Input() tableItems = [];
+  @Input() dataTable: DataTableViewModel;
   @Output() vanityIdToInteract = new EventEmitter<string>();
 
-  get keys() {
-    return this.tableColumns;
+  get columns() {
+    if (this.dataTable) {
+      return this.dataTable.columns;
+    }
   }
 
-  // TODO: Improve the DocumentStatus replacement
-  // The general idea is to replace the status numbers by their respective names, maybe with colors to better indicate status
-  // The aproach below works, but it's far from the best
-  get items() {
-    let tempItems = JSON.stringify(this.tableItems)
-      .replace(/"status":0/g, `"status":"Published"`)
-      .replace(/"status":1/g, `"status":"Pending approval"`)
-      .replace(/"status":2/g, `"status":"Draft"`);
-
-    return JSON.parse(tempItems);
+  get rows() {
+    if (this.dataTable) {
+      console.log(this.dataTable.rows[0].dataProperties.find(x => x.dataType.toLowerCase() == 'guid').dataContent);
+      return this.dataTable.rows;
+    }
   }
+
+  //// TODO: Improve the DocumentStatus replacement
+  //// The general idea is to replace the status numbers by their respective names, maybe with colors to better indicate status
+  //// The aproach below works, but it's far from the best
+  //get items() {
+  //  let tempItems = JSON.stringify(this.dataTable.data)
+  //    .replace(/"status":0/g, `"status":"Published"`)
+  //    .replace(/"status":1/g, `"status":"Pending approval"`)
+  //    .replace(/"status":2/g, `"status":"Draft"`);
+
+  //  return JSON.parse(tempItems);
+  //}
 
   public deleteItem(vanityId: string): void {
     if (confirm('Are you sure you want to delete this item?')) {
