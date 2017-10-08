92ec484a1c9d41198b209ea6c3438e5788840d00
Davidson Sousa <dev@davidsonsousa.net>
2017-10-08 13:56:46 +0200

    Refactoring Models

--- CmsEngine/Data/Models/Document.cs
+++ CmsEngine/Data/Models/Document.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
-using CmsEngine.Attributes;
 
 namespace CmsEngine.Data.Models
 {
@@ -12,28 +11,22 @@ namespace CmsEngine.Data.Models
             PublishedOn = DateTime.Now;
         }
 
-        [Searchable, Orderable, ShowOnDataTable(0)]
         [Required]
         [MaxLength(100, ErrorMessage = "The title must have less than 100 characters")]
         public string Title { get; set; }
 
-        [Searchable, Orderable, ShowOnDataTable(2)]
         public string Slug { get; set; }
 
-        [Searchable, Orderable, ShowOnDataTable(1)]
         [Required]
         [MaxLength(150, ErrorMessage = "The description must have less than 150 characters")]
         public string Description { get; set; }
 
         public string DocumentContent { get; set; }
 
-        [Searchable, Orderable, ShowOnDataTable(3)]
         public string Author { get; set; }
 
-        [Searchable, Orderable, ShowOnDataTable(5)]
         public DocumentStatus Status { get; set; }
 
-        [Searchable, Orderable, ShowOnDataTable(4)]
         [Required]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime PublishedOn { get; set; }
--- CmsEngine/Data/Models/Tag.cs
+++ CmsEngine/Data/Models/Tag.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using CmsEngine.Attributes;
 
 namespace CmsEngine.Data.Models
 {
@@ -15,12 +14,10 @@ namespace CmsEngine.Data.Models
 
         #endregion
 
-        [Searchable, Orderable, ShowOnDataTable(0)]
         [Required]
         [MaxLength(15, ErrorMessage = "The name must have less than 15 characters")]
         public string Name { get; set; }
 
-        [Searchable, Orderable, ShowOnDataTable(1)]
         public string Slug { get; set; }
     }
 }
