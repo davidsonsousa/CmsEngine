783f37f40a6df6b264c0de4acce65f1a4e27452a
Davidson Sousa <dev@davidsonsousa.net>
2017-10-17 22:56:50 +0200

    Adding an ASP.NET MVC project (CmsEngine.UI)

---
+++ CmsEngine.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CmsEngine.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
