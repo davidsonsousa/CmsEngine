713d68820686726734c4580d913f2e1947819514
Davidson Sousa <dev@davidsonsousa.net>
2017-11-07 01:01:01 +0100

    Adapting DataTableViewModel for datatables component

--- CmsEngine/Data/ViewModels/DataTableViewModel.cs
+++ CmsEngine/Data/ViewModels/DataTableViewModel.cs
@@ -1,26 +1,77 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace CmsEngine.Data.ViewModels
 {
     public class DataTableViewModel
     {
+        [JsonProperty(PropertyName = "draw")]
+        public int Draw { get; set; }
+
+        [JsonProperty(PropertyName = "recordsTotal")]
         public int RecordsTotal { get; set; }
 
+        [JsonProperty(PropertyName = "recordsFiltered")]
         public int RecordsFiltered { get; set; }
 
-        public List<string> Columns { get; set; }
-
-        public List<DataItem> Rows { get; set; }
+        [JsonProperty(PropertyName = "data")]
+        public List<List<string>> Data { get; set; }
     }
 
-    public class DataItem
+    public class DataTableParameters
     {
-        public List<DataProperty> DataProperties { get; set; }
+        [JsonProperty(PropertyName = "draw")]
+        public int Draw { get; set; }
+
+        [JsonProperty(PropertyName = "columns")]
+        public List<DataTableColumn> Columns { get; set; }
+
+        [JsonProperty(PropertyName = "order")]
+        public List<DataOrder> Order { get; set; }
+
+        [JsonProperty(PropertyName = "start")]
+        public int Start { get; set; }
+
+        [JsonProperty(PropertyName = "length")]
+        public int Length { get; set; }
+
+        [JsonProperty(PropertyName = "search")]
+        public Search Search { get; set; }
     }
 
-    public class DataProperty
+    public class Search
     {
-        public string DataType { get; set; }
-        public string DataContent { get; set; }
+        [JsonProperty(PropertyName = "regex")]
+        public bool Regex { get; set; }
+
+        [JsonProperty(PropertyName = "value")]
+        public string Value { get; set; }
+    }
+
+    public class DataTableColumn
+    {
+        [JsonProperty(PropertyName = "data")]
+        public int Data { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "searchable")]
+        public bool Searchable { get; set; }
+
+        [JsonProperty(PropertyName = "orderable")]
+        public bool Orderable { get; set; }
+
+        [JsonProperty(PropertyName = "search")]
+        public Search Search { get; set; }
+    }
+
+    public class DataOrder
+    {
+        [JsonProperty(PropertyName = "column")]
+        public int Column { get; set; }
+
+        [JsonProperty(PropertyName = "dir")]
+        public string Dir { get; set; }
     }
 }
