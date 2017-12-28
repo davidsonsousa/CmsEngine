2e01bda91360d7b06792ba4f39c678bd9b89c0bc
Davidson Sousa <dev@davidsonsousa.net>
2017-12-28 23:06:30 +0100

    Moving ApplicationUser to CmsEngine project

--- CmsEngine/Data/Models/ApplicationUser.cs
+++ CmsEngine/Data/Models/ApplicationUser.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
-namespace CmsEngine.Ui.Models
+namespace CmsEngine.Data.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
