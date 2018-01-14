2d5c16ce0912ac95f8303167761f857f2a850a1e
Davidson Sousa <dev@davidsonsousa.net>
2018-01-14 21:33:23 +0100

    Adding edit and view models for User

---
+++ CmsEngine/Data/EditModels/UserEditModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CmsEngine.Data.EditModels
+{
+    public class UserEditModel : BaseEditModel, IEditModel
+    {
+        public string FullName
+        {
+            get
+            {
+                return $"{Name} {Surname}";
+            }
+        }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ CmsEngine/Data/ViewModels/UserViewModel.cs
@@ -0,0 +1,18 @@
+using CmsEngine.Attributes;
+
+namespace CmsEngine.Data.ViewModels
+{
+    public class UserViewModel : BaseViewModel, IViewModel
+    {
+        [Searchable, Orderable, ShowOnDataTable(0)]
+        public string Name { get; set; }
+
+        [Searchable, Orderable, ShowOnDataTable(2)]
+        public string Surname { get; set; }
+
+        [Searchable, Orderable, ShowOnDataTable(1)]
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
