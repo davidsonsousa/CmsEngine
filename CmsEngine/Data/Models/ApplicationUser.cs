7b948f77787996f780d3b93ae18da0d1b75dc25c
Davidson Sousa <dev@davidsonsousa.net>
2018-01-14 21:32:57 +0100

    Adding properties: Name and Surname

--- CmsEngine/Data/Models/ApplicationUser.cs
+++ CmsEngine/Data/Models/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace CmsEngine.Data.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }
