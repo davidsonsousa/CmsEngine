835057ac686d666bed8908a477861d2ae7b57e7f
Davidson Sousa <dev@davidsonsousa.net>
2018-02-18 14:30:06 +0100

    Migrations - Added name and surname to user table

---
+++ CmsEngine/Migrations/20180218132611_ChangingUserProperties.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CmsEngine.Migrations
+{
+    public partial class ChangingUserProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "AspNetUsers");
+        }
+    }
+}
