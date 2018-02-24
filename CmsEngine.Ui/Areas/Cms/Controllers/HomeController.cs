2bdfdbfc73f8c547e1b01b752899a13e4d261ec5
Davidson Sousa <dev@davidsonsousa.net>
2018-02-24 20:14:52 +0100

    Inherited BaseController on HomeController

--- CmsEngine.Ui/Areas/Cms/Controllers/HomeController.cs
+++ CmsEngine.Ui/Areas/Cms/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
+using CmsEngine.Ui.Areas.Cms.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsEngine.Ui.Admin.Controllers
 {
     [Area("Cms")]
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public IActionResult Index()
         {
