ef3549acde62e6dacf49658be2b8e94682cd1a85
Davidson Sousa <dev@davidsonsousa.net>
2018-02-25 13:51:02 +0100

    Added DI constructor to HomeController

--- CmsEngine.Ui/Areas/Cms/Controllers/HomeController.cs
+++ CmsEngine.Ui/Areas/Cms/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
+using AutoMapper;
+using CmsEngine.Data.AccessLayer;
+using CmsEngine.Data.Models;
 using CmsEngine.Ui.Areas.Cms.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsEngine.Ui.Admin.Controllers
@@ -6,6 +11,11 @@ namespace CmsEngine.Ui.Admin.Controllers
     [Area("Cms")]
     public class HomeController : BaseController
     {
+        public HomeController(IUnitOfWork uow
+                            , IMapper mapper
+                            , IHttpContextAccessor hca
+                            , UserManager<ApplicationUser> userManager) : base(uow, mapper, hca, userManager) { }
+
         public IActionResult Index()
         {
             return View();
