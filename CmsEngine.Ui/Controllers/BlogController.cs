95da5e0de6dfb3a09e375a67ecdd5da2e097aa40
Davidson Sousa <dev@davidsonsousa.net>
2018-06-02 22:44:34 +0200

    Loaded content into ViewModel

--- CmsEngine.Ui/Controllers/BlogController.cs
+++ CmsEngine.Ui/Controllers/BlogController.cs
@@ -1,12 +1,33 @@
+using System.Collections.Generic;
+using AutoMapper;
+using CmsEngine.Data.AccessLayer;
+using CmsEngine.Data.Models;
+using CmsEngine.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsEngine.Ui.Controllers
 {
-    public class BlogController : Controller
+    public class BlogController : BaseController
     {
+        public BlogController(IUnitOfWork uow, IMapper mapper, IHttpContextAccessor hca, UserManager<ApplicationUser> userManager)
+                       : base(uow, mapper, hca, userManager)
+        {
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var blogViewModel = new BlogViewModel
+            {
+                PagedPosts = (IEnumerable<PostViewModel>)service.GetAllPostsReadOnly(4),
+                LatestPosts = (IEnumerable<PostViewModel>)service.GetAllPostsReadOnly(3),
+                Pages = (IEnumerable<PageViewModel>)service.GetAllPagesReadOnly(),
+                Categories = (IEnumerable<CategoryViewModel>)service.GetAllCategoriesReadOnly(),
+                Tags = (IEnumerable<TagViewModel>)service.GetAllTagsReadOnly()
+            };
+
+            return View(blogViewModel);
         }
     }
 }
