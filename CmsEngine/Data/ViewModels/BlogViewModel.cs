0c91761891838ac7f6ab2f98d5b57b4c02bf5759
Davidson Sousa <dev@davidsonsousa.net>
2018-06-02 22:41:45 +0200

    Renamed Posts to PagedPosts; Added LatestPosts

--- CmsEngine/Data/ViewModels/BlogViewModel.cs
+++ CmsEngine/Data/ViewModels/BlogViewModel.cs
@@ -4,7 +4,8 @@ namespace CmsEngine.Data.ViewModels
 {
     public class BlogViewModel
     {
-        public IEnumerable<PostViewModel> Posts { get; set; }
+        public IEnumerable<PostViewModel> PagedPosts { get; set; }
+        public IEnumerable<PostViewModel> LatestPosts { get; set; }
         public IEnumerable<PageViewModel> Pages { get; set; }
         public IEnumerable<CategoryViewModel> Categories { get; set; }
         public IEnumerable<TagViewModel> Tags { get; set; }
