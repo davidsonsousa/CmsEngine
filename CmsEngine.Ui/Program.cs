5c50f71c34fc185cc60fcd0ade044a543bfec88f
Davidson Sousa <dev@davidsonsousa.net>
2018-06-02 22:00:15 +0200

    Made Program class static

--- CmsEngine.Ui/Program.cs
+++ CmsEngine.Ui/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace CmsEngine.Ui
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
