27ca17e895fb6b7675ef3fc423db764de0cc27de
Davidson Sousa <dev@davidsonsousa.net>
2018-06-03 12:47:30 +0200

    Renamed BlogViewModel to InstanceViewModel and added extra properties

--- CmsEngine/Data/ViewModels/InstanceViewModel.cs
+++ CmsEngine/Data/ViewModels/InstanceViewModel.cs
@@ -2,14 +2,24 @@ using System.Collections.Generic;
 
 namespace CmsEngine.Data.ViewModels
 {
-    public class BlogViewModel
+    public class InstanceViewModel
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Culture { get; set; }
+
+        public string UrlFormat { get; set; }
+
+        public string DateFormat { get; set; }
+
+        public string SiteUrl { get; set; }
+
         public IEnumerable<PostViewModel> PagedPosts { get; set; }
         public IEnumerable<PostViewModel> LatestPosts { get; set; }
         public IEnumerable<PageViewModel> Pages { get; set; }
         public IEnumerable<CategoryViewModel> Categories { get; set; }
         public IEnumerable<TagViewModel> Tags { get; set; }
-
-        public WebsiteViewModel Instance { get; set; }
     }
 }
