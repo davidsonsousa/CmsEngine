8d417311bbe0bc4d4e3da17ce172c3882d1a07d1
Davidson Sousa <dev@davidsonsousa.net>
2018-06-03 17:27:16 +0200

    Added method to load data into instance

--- CmsEngine.Ui/Controllers/BaseController.cs
+++ CmsEngine.Ui/Controllers/BaseController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AutoMapper;
 using CmsEngine.Data.AccessLayer;
 using CmsEngine.Data.Models;
@@ -5,6 +6,7 @@ using CmsEngine.Data.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CmsEngine.Ui.Controllers
 {
@@ -17,6 +19,17 @@ namespace CmsEngine.Ui.Controllers
         {
             service = new CmsService(uow, mapper, hca, userManager);
             instance = service.Instance;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            instance.PagedPosts = (IEnumerable<PostViewModel>)service.GetAllPostsReadOnly(4);
+            instance.LatestPosts = (IEnumerable<PostViewModel>)service.GetAllPostsReadOnly(3);
+            instance.Pages = (IEnumerable<PageViewModel>)service.GetAllPagesReadOnly();
+            instance.Categories = (IEnumerable<CategoryViewModel>)service.GetAllCategoriesReadOnly();
+            instance.Tags = (IEnumerable<TagViewModel>)service.GetAllTagsReadOnly();
         }
     }
 }
