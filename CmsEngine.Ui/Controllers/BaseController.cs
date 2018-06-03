6c4b8f919eb0f97d102f12b83ae794f369147c9d
Davidson Sousa <dev@davidsonsousa.net>
2018-06-03 13:22:24 +0200

    Cleaning up

--- CmsEngine.Ui/Controllers/BaseController.cs
+++ CmsEngine.Ui/Controllers/BaseController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CmsEngine.Data.AccessLayer;
 using CmsEngine.Data.Models;
+using CmsEngine.Data.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace CmsEngine.Ui.Controllers
     public class BaseController : Controller
     {
         protected readonly CmsService service;
+        protected readonly InstanceViewModel instance;
 
         public BaseController(IUnitOfWork uow, IMapper mapper, IHttpContextAccessor hca, UserManager<ApplicationUser> userManager)
         {
             service = new CmsService(uow, mapper, hca, userManager);
+            instance = service.Instance;
         }
     }
 }
--- CmsEngine.Ui/Controllers/BlogController.cs
+++ CmsEngine.Ui/Controllers/BlogController.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
 using AutoMapper;
 using CmsEngine.Data.AccessLayer;
 using CmsEngine.Data.Models;
-using CmsEngine.Data.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -18,16 +16,7 @@ namespace CmsEngine.Ui.Controllers
 
         public IActionResult Index()
         {
-            var blogViewModel = new InstanceViewModel
-            {
-                PagedPosts = (IEnumerable<PostViewModel>)service.GetAllPostsReadOnly(4),
-                LatestPosts = (IEnumerable<PostViewModel>)service.GetAllPostsReadOnly(3),
-                Pages = (IEnumerable<PageViewModel>)service.GetAllPagesReadOnly(),
-                Categories = (IEnumerable<CategoryViewModel>)service.GetAllCategoriesReadOnly(),
-                Tags = (IEnumerable<TagViewModel>)service.GetAllTagsReadOnly()
-            };
-
-            return View(blogViewModel);
+            return View(instance);
         }
     }
 }
