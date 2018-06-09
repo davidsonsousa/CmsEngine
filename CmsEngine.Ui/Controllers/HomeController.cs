508a8c4d8e22bb773bf9718df0d036b7e29a84fe
Davidson Sousa <dev@davidsonsousa.net>
2018-06-09 13:21:00 +0200

    Added instance into HomeController via BaseController

--- CmsEngine.Ui/Controllers/HomeController.cs
+++ CmsEngine.Ui/Controllers/HomeController.cs
@@ -1,28 +1,38 @@
 using System.Diagnostics;
+using AutoMapper;
+using CmsEngine.Data.AccessLayer;
+using CmsEngine.Data.Models;
 using CmsEngine.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsEngine.Ui.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        public HomeController(IUnitOfWork uow, IMapper mapper, IHttpContextAccessor hca, UserManager<ApplicationUser> userManager)
+                       : base(uow, mapper, hca, userManager)
+        {
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(instance);
         }
 
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
-            return View();
+            return View(instance);
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
-            return View();
+            return View(instance);
         }
 
         public IActionResult Error()
