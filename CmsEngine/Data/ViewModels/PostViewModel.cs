75c235260f0c89464a7999f496bf2a69dfbc1c59
Davidson Sousa <dev@davidsonsousa.net>
2018-06-10 18:18:48 +0200

    Added Categories and Tags into PostViewModel

--- CmsEngine/Data/ViewModels/PostViewModel.cs
+++ CmsEngine/Data/ViewModels/PostViewModel.cs
@@ -1,21 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 namespace CmsEngine.Data.ViewModels
 {
     public class PostViewModel : BaseViewModel
     {
         public string Title { get; set; }
-
         public string Slug { get; set; }
-
         public string Description { get; set; }
-
         public string DocumentContent { get; set; }
-
         public string Author { get; set; }
-
         public DocumentStatus Status { get; set; }
-
         public DateTime PublishedOn { get; set; }
+        public IEnumerable<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+        public IEnumerable<TagViewModel> Tags { get; set; } = new List<TagViewModel>();
     }
 }
+
+
