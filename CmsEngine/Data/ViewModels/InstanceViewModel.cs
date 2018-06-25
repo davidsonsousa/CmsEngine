4de7585e04fe3abc01b6437d3cd84397c118110e
Davidson Sousa <dev@davidsonsousa.net>
2018-06-25 21:44:21 +0200

    Changed type of PagedPosts from IEnumerable to PaginatedList

--- CmsEngine/Data/ViewModels/InstanceViewModel.cs
+++ CmsEngine/Data/ViewModels/InstanceViewModel.cs
@@ -5,20 +5,16 @@ namespace CmsEngine.Data.ViewModels
     public class InstanceViewModel
     {
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public string Culture { get; set; }
-
         public string UrlFormat { get; set; }
-
         public string DateFormat { get; set; }
-
         public string SiteUrl { get; set; }
+        public int ArticleLimit { get; set; }
 
         public PostViewModel SelectedPost { get; set; }
 
-        public IEnumerable<PostViewModel> PagedPosts { get; set; }
+        public PaginatedList<PostViewModel> PagedPosts { get; set; }
         public IEnumerable<PostViewModel> LatestPosts { get; set; }
         public IEnumerable<PageViewModel> Pages { get; set; }
         public IEnumerable<CategoryViewModel> Categories { get; set; }
