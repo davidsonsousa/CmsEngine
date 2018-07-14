3ff5c6edaa4d3917953b94acf987f8f56812ae29
Davidson Sousa <dev@davidsonsousa.net>
2018-07-15 01:50:53 +0200

    Added a class to represent the contact form

---
+++ CmsEngine/Helpers/Email/ContactForm.cs
@@ -0,0 +1,9 @@
+namespace CmsEngine.Helpers.Email
+{
+    public class ContactForm
+    {
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
