79374e3241f61ba2a557688d34cf70cbe053da51
Davidson Sousa <dev@davidsonsousa.net>
2018-09-01 19:35:05 +0200

    Renamed many to many tables

--- CmsEngine/Data/Models/PostApplicationUser.cs
+++ CmsEngine/Data/Models/PostApplicationUser.cs
@@ -1,11 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CmsEngine.Data.Models
 {
+    [Table("PostAspNetUser")]
     public class PostApplicationUser
     {
         public int PostId { get; set; }
         public virtual Post Post { get; set; }
 
         public string ApplicationUserId { get; set; }
-        public virtual ApplicationUser ApplicationUser{ get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
