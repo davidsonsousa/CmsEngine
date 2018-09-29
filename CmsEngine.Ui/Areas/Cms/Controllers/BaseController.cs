42e39cb139af01b8d5f90ebc94bc912376bb8397
Davidson Sousa <dev@davidsonsousa.net>
2018-09-29 23:20:52 +0200

    Added file upload action method

--- CmsEngine.Ui/Areas/Cms/Controllers/BaseController.cs
+++ CmsEngine.Ui/Areas/Cms/Controllers/BaseController.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using AutoMapper;
 using CmsEngine.Data.AccessLayer;
 using CmsEngine.Data.Models;
+using CmsEngine.Helpers;
+using CmsEngine.Utils;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
 
 namespace CmsEngine.Ui.Areas.Cms.Controllers
 {
@@ -13,6 +20,9 @@ namespace CmsEngine.Ui.Areas.Cms.Controllers
     public class BaseController : Controller
     {
         protected readonly CmsService service;
+        protected string filePath;
+        protected List<UploadFilesResult> fileList;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
         public BaseController()
         {
@@ -22,6 +32,13 @@ namespace CmsEngine.Ui.Areas.Cms.Controllers
         public BaseController(IUnitOfWork uow, IMapper mapper, IHttpContextAccessor hca, UserManager<ApplicationUser> userManager)
         {
             service = new CmsService(uow, mapper, hca, userManager);
+        }
+
+        public BaseController(IUnitOfWork uow, IMapper mapper, IHttpContextAccessor hca,
+                              UserManager<ApplicationUser> userManager, IHostingEnvironment hostingEnvironment)
+        {
+            service = new CmsService(uow, mapper, hca, userManager);
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public override void OnActionExecuting(ActionExecutingContext context)
@@ -52,6 +69,64 @@ namespace CmsEngine.Ui.Areas.Cms.Controllers
             }
 
             TempData["DangerMessage"] = generalError;
+        }
+
+        [HttpPost]
+        public async virtual Task<ContentResult> UploadFiles()
+        {
+            string folder = Path.Combine(_hostingEnvironment.WebRootPath, "UploadedFiles", filePath);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            fileList = new List<UploadFilesResult>();
+
+            foreach (var formFile in Request.Form.Files)
+            {
+                if (formFile.Length == 0)
+                {
+                    continue;
+                }
+
+                using (var stream = new FileStream(Path.Combine(folder, formFile.FileName), FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+
+                bool isImage = FileHelper.IsImage(formFile.FileName);
+
+                string savedFileName = Path.Combine(folder, Path.GetFileName(formFile.FileName));
+
+                string fileSize = FileHelper.FormatFileSize(savedFileName);
+                string pathUrl = "";
+
+                if (isImage)
+                {
+                    // Generate the thumbnails for the images
+                    string thumbnailFileName = Path.Combine(folder, "tn_" + formFile.FileName);
+                    FileHelper.ResizeImage(savedFileName, thumbnailFileName, 316, 198, true);
+
+                    pathUrl = $"/image/{filePath}/";
+                }
+                else
+                {
+                    pathUrl = $"/file/{filePath}/";
+                }
+
+                fileList.Add(new UploadFilesResult()
+                {
+                    FileName = formFile.FileName,
+                    ThumbnailName = "tn_" + formFile.FileName,
+                    Path = pathUrl,
+                    Length = formFile.Length,
+                    ContentType = formFile.ContentType,
+                    IsImage = isImage,
+                    Size = fileSize
+                });
+            }
+            return Content(JsonConvert.SerializeObject(fileList).ToLowerInvariant(), "application/json");
         }
     }
 }
