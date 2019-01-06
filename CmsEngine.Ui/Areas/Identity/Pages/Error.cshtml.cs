ddbf6c7335eb1b69403655a8cdeb9618c2915b54
Davidson Sousa <dev@davidsonsousa.net>
2019-01-06 14:32:49 +0100

    Removed ResponseCacheAttribute

--- CmsEngine.Ui/Areas/Identity/Pages/Error.cshtml.cs
+++ CmsEngine.Ui/Areas/Identity/Pages/Error.cshtml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CmsEngine.Ui.Areas.Identity.Pages
 {
@@ -17,7 +11,6 @@ namespace CmsEngine.Ui.Areas.Identity.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
