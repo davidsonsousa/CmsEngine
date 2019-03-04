16cdda366fae66664b9d3d749790ad6c95b40caa
Davidson Sousa <dev@davidsonsousa.net>
2019-03-04 20:44:02 +0100

    Replaced PostCount by Posts

--- CmsEngine/Data/ViewModels/CategoryViewModel.cs
+++ CmsEngine/Data/ViewModels/CategoryViewModel.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace CmsEngine.Data.ViewModels
 {
     public class CategoryViewModel : BaseViewModel, IViewModel
@@ -5,6 +7,6 @@ namespace CmsEngine.Data.ViewModels
         public string Name { get; set; }
         public string Slug { get; set; }
         public string Description { get; set; }
-        public int PostCount { get; set; }
+        public IEnumerable<PostViewModel> Posts { get; set; }
     }
 }
