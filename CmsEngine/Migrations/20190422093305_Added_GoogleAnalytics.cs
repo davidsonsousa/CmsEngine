3d846a74c9e96d0e0453bfa3f6bdae6c68f892d0
Davidson Sousa <dev@davidsonsousa.net>
2019-04-22 11:35:46 +0200

    Added DB migration

---
+++ CmsEngine/Migrations/20190422093305_Added_GoogleAnalytics.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CmsEngine.Migrations
+{
+    public partial class Added_GoogleAnalytics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleAnalytics",
+                table: "Websites",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoogleAnalytics",
+                table: "Websites");
+        }
+    }
+}
