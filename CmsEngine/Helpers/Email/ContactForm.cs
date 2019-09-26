fce86952ed1c1a54ba44e355eb044e1c8cf2c2eb
Davidson Sousa <davidson.sousa@gmail.com>
2019-09-27 00:58:42 +0200

    Removed helpers

--- CmsEngine/Helpers/Email/ContactForm.cs
+++
@@ -1,9 +0,0 @@
-namespace CmsEngine.Helpers.Email
-{
-    public class ContactForm
-    {
-        public string Sender { get; set; }
-        public string Subject { get; set; }
-        public string Message { get; set; }
-    }
-}
--- CmsEngine/Helpers/Email/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace CmsEngine.Helpers.Email
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(ContactForm mailEditModel);
-    }
-}
