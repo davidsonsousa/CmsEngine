38c097bde4f0dae6849f5e443dcf34478f62ff99
Davidson Sousa <davidson.sousa@gmail.com>
2019-10-04 19:53:48 +0200

    Added attributes

---
+++ CmsEngine.Application/Attributes/Searchable.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CmsEngine.Domain.Attributes
+{
+    /// <summary>
+    /// Enables the property to be searchable in the Search functionality
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class Searchable : Attribute
+    {
+
+    }
+}
