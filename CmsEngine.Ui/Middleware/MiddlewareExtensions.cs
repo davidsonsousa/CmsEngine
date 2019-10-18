dd515d70580d30e5e53fbd7ad695449745ba2c7d
Davidson Sousa <dev@davidsonsousa.net>
2019-10-19 00:45:56 +0200

    Added new Middleware for security headers

--- CmsEngine.Ui/Middleware/MiddlewareExtensions.cs
+++ CmsEngine.Ui/Middleware/MiddlewareExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+using CmsEngine.Ui.Middleware.SecurityHeaders;
 using Microsoft.AspNetCore.Builder;
 
 namespace CmsEngine.Ui.Middleware
@@ -6,7 +8,32 @@ namespace CmsEngine.Ui.Middleware
     {
         public static IApplicationBuilder ConfigureFileUpload(this IApplicationBuilder builder, FileUploadOptions options)
         {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
             return builder.UseMiddleware<ConfigureFileUploadMiddleware>(options);
+        }
+
+        public static IApplicationBuilder UseSecurityHeaders(this IApplicationBuilder builder, SecurityHeadersBuilder securityHeadersBuilder)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (securityHeadersBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(securityHeadersBuilder));
+            }
+
+            return builder.UseMiddleware<SecurityHeadersMiddleware>(securityHeadersBuilder.Build());
         }
     }
 }
