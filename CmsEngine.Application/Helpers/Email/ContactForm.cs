6e43db84c5a005e876a85747d286a7a3eff2e044
Davidson Sousa <dev@davidsonsousa.net>
2019-10-19 10:53:14 +0200

    Improved ContactForm

--- CmsEngine.Application/Helpers/Email/ContactForm.cs
+++ CmsEngine.Application/Helpers/Email/ContactForm.cs
@@ -1,16 +1,34 @@
+using Newtonsoft.Json.Linq;
+
 namespace CmsEngine.Application.Helpers.Email
 {
     public class ContactForm
     {
-        public string Sender { get; }
+        public string From { get; }
+        public string To { get; }
         public string Subject { get; }
         public string Message { get; }
 
-        public ContactForm(string sender, string subject, string message)
+        public ContactForm(string from, string to, string subject, string message) : this(to, subject, message)
         {
-            Sender = sender;
+            From = from;
+        }
+
+        public ContactForm(string to, string subject, string message)
+        {
+            To = to;
             Subject = subject;
             Message = message;
+        }
+
+        public override string ToString()
+        {
+            var jsonResult = new JObject(
+                                        new JProperty("From", From),
+                                        new JProperty("Subject", Subject),
+                                        new JProperty("Message", Message)
+                                    );
+            return jsonResult.ToString();
         }
     }
 }
