1942cf103abc6b933e76636afb6a178fd59039ee
Davidson Sousa <dev@davidsonsousa.net>
2019-10-20 20:17:49 +0200

    Created new migration to add Google related fields

--- CmsEngine.Data/Migrations/20191020174931_GoogleRecaptchaFields.Designer.cs
+++ CmsEngine.Data/Migrations/20191020174931_GoogleRecaptchaFields.Designer.cs
@@ -4,14 +4,16 @@ using CmsEngine.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CmsEngine.Data.Migrations
 {
     [DbContext(typeof(CmsEngineContext))]
-    partial class CmsEngineContextModelSnapshot : ModelSnapshot
+    [Migration("20191020174931_GoogleRecaptchaFields")]
+    partial class GoogleRecaptchaFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
