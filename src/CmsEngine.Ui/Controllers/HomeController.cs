d373d286ac90c9880d281d008d27b8dcda501f11
Davidson Sousa <davidsonsousa@users.noreply.github.com>
2023-06-19 22:14:05 +0200

    Feature/remove contact form (#120)
    * Add empty connection strings
    
    * Removed Contact form and references
    
    * Redirected contact route to index

--- src/CmsEngine.Ui/Controllers/HomeController.cs
+++ src/CmsEngine.Ui/Controllers/HomeController.cs
@@ -2,20 +2,15 @@ namespace CmsEngine.Ui.Controllers;
 
 public class HomeController : BaseController
 {
-    private readonly ICmsEngineEmailSender _emailSender;
     private readonly IPageService _pageService;
     private readonly IXmlService _xmlService;
-    private readonly IEmailService _emailService;
 
-    public HomeController(ILoggerFactory loggerFactory, ICmsEngineEmailSender emailSender, IPageService pageService, IXmlService xmlService,
-                          ICategoryService categoryService, ITagService tagService, IService service, IPostService postService,
-                          IEmailService emailService)
+    public HomeController(ILoggerFactory loggerFactory, IPageService pageService, IXmlService xmlService,
+                          ICategoryService categoryService, ITagService tagService, IService service, IPostService postService)
                          : base(loggerFactory, service, categoryService, pageService, postService, tagService)
     {
-        _emailSender = emailSender;
         _pageService = pageService;
         _xmlService = xmlService;
-        _emailService = emailService;
     }
 
     public IActionResult Index()
@@ -41,41 +36,6 @@ public class HomeController : BaseController
     {
         Instance.PageTitle = $"Archive - {Instance.Name}";
         return View(Instance);
-    }
-
-    public IActionResult Contact()
-    {
-        Instance.PageTitle = $"Contact - {Instance.Name}";
-        return View(Instance);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> ContactAsync(ContactForm contactForm)
-    {
-        if (!ModelState.IsValid)
-        {
-            TempData[MessageConstants.WarningMessage] = "Please double check the information in the form and try again.";
-            return View(Instance);
-        }
-
-        contactForm.To = Instance.ContactDetails?.Email;
-
-        try
-        {
-            if ((await _emailService.Save(contactForm)).IsError)
-            {
-                throw new Exception("Error when saving e-mail");
-            }
-
-            await _emailSender.SendEmailAsync(contactForm);
-            TempData[MessageConstants.SuccessMessage] = "Your message was sent. I will answer as soon as I can.";
-        }
-        catch (EmailException)
-        {
-            TempData[MessageConstants.DangerMessage] = "We could not send the messsage. Please try other communication channels.";
-        }
-
-        return RedirectToAction(nameof(HomeController.Contact));
     }
 
     public async Task<IActionResult> SitemapAsync()
