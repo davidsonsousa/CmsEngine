12f717ca3a94c033fa76b1560a265bf5ea23c7c0
Davidson Sousa <dev@davidsonsousa.net>
2023-08-06 16:55:44 +0200

    Code refactor

--- src/CmsEngine.Application/Extensions/Mapper/PostExtensions.cs
+++ src/CmsEngine.Application/Extensions/Mapper/PostExtensions.cs
@@ -32,25 +32,18 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostEditModel> MapToEditModel(this IEnumerable<Post> posts)
     {
-        var editModels = new List<PostEditModel>();
-
-        foreach (var item in posts)
+        return posts.Select(item => new PostEditModel
         {
-            editModels.Add(new PostEditModel
-            {
-                Id = item.Id,
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Slug = item.Slug,
-                Description = item.Description,
-                DocumentContent = item.DocumentContent,
-                HeaderImage = item.HeaderImage,
-                PublishedOn = item.PublishedOn,
-                Status = item.Status
-            });
-        }
-
-        return editModels;
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Title = item.Title,
+            Slug = item.Slug,
+            Description = item.Description,
+            DocumentContent = item.DocumentContent,
+            HeaderImage = item.HeaderImage,
+            PublishedOn = item.PublishedOn,
+            Status = item.Status
+        }).ToList();
     }
 
     /// <summary>
@@ -102,23 +95,17 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostTableViewModel> MapToTableViewModel(this IEnumerable<Post> posts)
     {
-        var tableViewModel = new List<PostTableViewModel>();
-
-        foreach (var item in posts)
-        {
-            tableViewModel.Add(new PostTableViewModel
-            {
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Description = item.Description,
-                Slug = item.Slug,
-                PublishedOn = item.PublishedOn.ToString("yyyy-MM-dd HH:mm:ss"),
-                Status = item.Status,
-                Author = item.ApplicationUsers.MapToViewModelSimple().Single()
-            });
-        }
-
-        return tableViewModel;
+        return (from item in posts
+                select new PostTableViewModel
+                {
+                    VanityId = item.VanityId,
+                    Title = item.Title,
+                    Description = item.Description,
+                    Slug = item.Slug,
+                    PublishedOn = item.PublishedOn.ToString("yyyy-MM-dd HH:mm:ss"),
+                    Status = item.Status,
+                    Author = item.ApplicationUsers.MapToViewModelSimple().Single()
+                }).ToList();
     }
 
     /// <summary>
@@ -149,25 +136,18 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostViewModel> MapToViewModel(this IEnumerable<Post> posts, string dateFormat)
     {
-        var viewModels = new List<PostViewModel>();
-
-        foreach (var item in posts)
+        return posts.Select(item => new PostViewModel
         {
-            viewModels.Add(new PostViewModel
-            {
-                Id = item.Id,
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Slug = item.Slug,
-                Description = item.Description,
-                DocumentContent = item.DocumentContent,
-                HeaderImage = item.HeaderImage,
-                PublishedOn = item.PublishedOn.ToString(dateFormat),
-                Status = item.Status
-            });
-        }
-
-        return viewModels;
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Title = item.Title,
+            Slug = item.Slug,
+            Description = item.Description,
+            DocumentContent = item.DocumentContent,
+            HeaderImage = item.HeaderImage,
+            PublishedOn = item.PublishedOn.ToString(dateFormat),
+            Status = item.Status
+        }).ToList();
     }
 
     /// <summary>
@@ -177,26 +157,19 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostViewModel> MapToViewModelWithCategories(this IEnumerable<Post> posts, string dateFormat)
     {
-        var viewModels = new List<PostViewModel>();
-
-        foreach (var item in posts)
+        return posts.Select(item => new PostViewModel
         {
-            viewModels.Add(new PostViewModel
-            {
-                Id = item.Id,
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Slug = item.Slug,
-                Description = item.Description,
-                DocumentContent = item.DocumentContent,
-                HeaderImage = item.HeaderImage,
-                PublishedOn = item.PublishedOn.ToString(dateFormat),
-                Status = item.Status,
-                Categories = item.PostCategories.Select(x => x.Category).MapToViewModel(dateFormat)
-            });
-        }
-
-        return viewModels;
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Title = item.Title,
+            Slug = item.Slug,
+            Description = item.Description,
+            DocumentContent = item.DocumentContent,
+            HeaderImage = item.HeaderImage,
+            PublishedOn = item.PublishedOn.ToString(dateFormat),
+            Status = item.Status,
+            Categories = item.PostCategories.Select(x => x.Category).MapToViewModel(dateFormat)
+        }).ToList();
     }
 
     /// <summary>
@@ -206,26 +179,19 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostViewModel> MapToViewModelWithTags(this IEnumerable<Post> posts, string dateFormat)
     {
-        var viewModels = new List<PostViewModel>();
-
-        foreach (var item in posts)
+        return posts.Select(item => new PostViewModel
         {
-            viewModels.Add(new PostViewModel
-            {
-                Id = item.Id,
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Slug = item.Slug,
-                Description = item.Description,
-                DocumentContent = item.DocumentContent,
-                HeaderImage = item.HeaderImage,
-                PublishedOn = item.PublishedOn.ToString(dateFormat),
-                Status = item.Status,
-                Tags = item.PostTags.Select(x => x.Tag).MapToViewModel()
-            });
-        }
-
-        return viewModels;
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Title = item.Title,
+            Slug = item.Slug,
+            Description = item.Description,
+            DocumentContent = item.DocumentContent,
+            HeaderImage = item.HeaderImage,
+            PublishedOn = item.PublishedOn.ToString(dateFormat),
+            Status = item.Status,
+            Tags = item.PostTags.Select(x => x.Tag).MapToViewModel()
+        }).ToList();
     }
 
     /// <summary>
@@ -235,24 +201,17 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostViewModel> MapToViewModelForPartialView(this IEnumerable<Post> posts, string dateFormat)
     {
-        var viewModels = new List<PostViewModel>();
-
-        foreach (var item in posts)
+        return posts.Select(item => new PostViewModel
         {
-            viewModels.Add(new PostViewModel
-            {
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Slug = item.Slug,
-                Description = item.Description,
-                HeaderImage = item.HeaderImage,
-                PublishedOn = item.PublishedOn.ToString(dateFormat),
-                Categories = item.Categories.MapToViewModelSimple(),
-                Author = item.ApplicationUsers.MapToViewModelSimple().Single()
-            });
-        }
-
-        return viewModels;
+            VanityId = item.VanityId,
+            Title = item.Title,
+            Slug = item.Slug,
+            Description = item.Description,
+            HeaderImage = item.HeaderImage,
+            PublishedOn = item.PublishedOn.ToString(dateFormat),
+            Categories = item.Categories.MapToViewModelSimple(),
+            Author = item.ApplicationUsers.MapToViewModelSimple().Single()
+        }).ToList();
     }
 
     /// <summary>
@@ -262,24 +221,17 @@ public static class PostExtensions
     /// <returns></returns>
     public static IEnumerable<PostViewModel> MapToViewModelForPartialViewForTags(this IEnumerable<Post> posts, string dateFormat)
     {
-        var viewModels = new List<PostViewModel>();
-
-        foreach (var item in posts)
+        return posts.Select(item => new PostViewModel
         {
-            viewModels.Add(new PostViewModel
-            {
-                VanityId = item.VanityId,
-                Title = item.Title,
-                Slug = item.Slug,
-                Description = item.Description,
-                HeaderImage = item.HeaderImage,
-                PublishedOn = item.PublishedOn.ToString(dateFormat),
-                Categories = item.Categories.MapToViewModelSimple(),
-                Tags = item.Tags.MapToViewModelSimple(),
-                Author = item.ApplicationUsers.MapToViewModelSimple().Single()
-            });
-        }
-
-        return viewModels;
+            VanityId = item.VanityId,
+            Title = item.Title,
+            Slug = item.Slug,
+            Description = item.Description,
+            HeaderImage = item.HeaderImage,
+            PublishedOn = item.PublishedOn.ToString(dateFormat),
+            Categories = item.Categories.MapToViewModelSimple(),
+            Tags = item.Tags.MapToViewModelSimple(),
+            Author = item.ApplicationUsers.MapToViewModelSimple().Single()
+        }).ToList();
     }
 }
