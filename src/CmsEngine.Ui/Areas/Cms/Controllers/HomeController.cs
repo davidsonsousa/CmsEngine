c5680803645446fad43cedc3b267c7fa3d9f38cd
Davidson Sousa <dev@davidsonsousa.net>
2024-08-15 23:19:13 +0200

    Add counter in the dashboard

--- src/CmsEngine.Ui/Areas/Cms/Controllers/HomeController.cs
+++ src/CmsEngine.Ui/Areas/Cms/Controllers/HomeController.cs
@@ -3,17 +3,34 @@ namespace CmsEngine.Ui.Areas.Cms.Controllers;
 [Area("Cms")]
 public class HomeController : BaseController
 {
-    private readonly IEmailService _emailService;
+    private readonly ICategoryService _categoryService;
+    private readonly IPageService _pageService;
+    private readonly IPostService _postService;
+    private readonly ITagService _tagService;
 
-    public HomeController(ILoggerFactory loggerFactory, IService service, IEmailService emailService)
+    public HomeController(ILoggerFactory loggerFactory, IService service, ICategoryService categoryService,
+                          IPageService pageService, IPostService postService, ITagService tagService)
                          : base(loggerFactory, service)
     {
-        _emailService = emailService;
+        _categoryService = categoryService;
+        _pageService = pageService;
+        _postService = postService;
+        _tagService = tagService;
+
     }
 
     public async Task<IActionResult> IndexAsync()
     {
         SetupMessages("Dashboard");
-        return View(await _emailService.GetOrderedByDate());
+
+        var homeViewModel = new HomeViewModel
+        {
+            CategoryCount = await _categoryService.GetCategoryCount(),
+            PageCount = await _pageService.GetPageCount(),
+            PostCount = await _postService.GetPostCount(),
+            TagCount = await _tagService.GetTagCount()
+        };
+
+        return View(homeViewModel);
     }
 }
