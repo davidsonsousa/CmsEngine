86b831ac8f6d1472fe56927a9b1cee7b985e5136
Davidson Sousa <davidsonsousa@users.noreply.github.com>
2025-06-01 19:12:55 +0200

    Feature/code improvements (#143)
    * Improve Save method in the unit of work
    
    * Improvements on SaveChangesAsync
    
    * Clean GlobalUsings

--- src/CmsEngine.Data/GlobalUsings.cs
+++ src/CmsEngine.Data/GlobalUsings.cs
@@ -13,3 +13,4 @@ global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Logging;
