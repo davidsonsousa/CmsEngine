8a962558612ac149b860cd1b99a5798d775663b0
Davidson Sousa <dev@davidsonsousa.net>
2025-09-06 00:34:47 +0200

    Improve BaseServiceTests and minor refactoring

--- tests/CmsEngine.Tests/Services/BaseServiceTests.cs
+++ tests/CmsEngine.Tests/Services/BaseServiceTests.cs
@@ -1,7 +1,4 @@
-﻿using CmsEngine.Application.Services.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace CmsEngine.Tests.Services;
+﻿namespace CmsEngine.Tests.Services;
 
 public class BaseServiceTests
 {
@@ -14,11 +11,18 @@ public class BaseServiceTests
     protected readonly Mock<IEmailRepository> _emailRepoMock;
     protected readonly Mock<IWebsiteRepository> _websiteRepoMock;
     protected readonly Mock<ICategoryRepository> _categoryRepoMock;
+    protected readonly Mock<ITagRepository> _tagRepoMock;
+    protected readonly Mock<IPageRepository> _pageRepoMock;
+    protected readonly Mock<IPostRepository> _postRepoMock;
 
     protected readonly Mock<ICacheService> _cacheServiceMock;
     protected readonly IEmailService _emailService;
     protected readonly IWebsiteService _websiteService;
     protected readonly ICategoryService _categoryService;
+    protected readonly ITagService _tagService;
+    protected readonly IPageService _pageService;
+    protected readonly IPostService _postService;
+    protected readonly IXmlService _xmlService;
 
     public BaseServiceTests()
     {
@@ -34,11 +38,36 @@ public class BaseServiceTests
         _emailRepoMock = new Mock<IEmailRepository>();
         _websiteRepoMock = new Mock<IWebsiteRepository>();
         _categoryRepoMock = new Mock<ICategoryRepository>();
+        _tagRepoMock = new Mock<ITagRepository>();
+        _pageRepoMock = new Mock<IPageRepository>();
+        _postRepoMock = new Mock<IPostRepository>();
+
+        var user = new ApplicationUser
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "testuser"
+        };
+        _userManagerMock.Setup(u => u.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(user);
+
+        var website = new Website
+        {
+            Id = 1,
+            Name = "TestSite",
+            Tagline = "Test Tagline",
+            Culture = "en-US",
+            UrlFormat = "[site_url]/[type]/[slug]",
+            DateFormat = "yyyy-MM-dd",
+            SiteUrl = "https://test.com"
+        };
+        _websiteRepoMock.Setup(r => r.GetWebsiteInstanceByHost(It.IsAny<string>())).Returns(website);
 
         _uowMock.Setup(u => u.Users).Returns(_userManagerMock.Object);
         _uowMock.Setup(u => u.Emails).Returns(_emailRepoMock.Object);
         _uowMock.Setup(u => u.Websites).Returns(_websiteRepoMock.Object);
         _uowMock.Setup(u => u.Categories).Returns(_categoryRepoMock.Object);
+        _uowMock.Setup(u => u.Tags).Returns(_tagRepoMock.Object);
+        _uowMock.Setup(u => u.Pages).Returns(_pageRepoMock.Object);
+        _uowMock.Setup(u => u.Posts).Returns(_postRepoMock.Object);
 
         _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
         var context = new DefaultHttpContext();
@@ -58,5 +87,9 @@ public class BaseServiceTests
         _emailService = new Mock<EmailService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
         _websiteService = new Mock<WebsiteService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
         _categoryService = new Mock<CategoryService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
+        _tagService = new Mock<TagService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
+        _pageService = new Mock<PageService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
+        _postService = new Mock<PostService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
+        _xmlService = new Mock<XmlService>(_uowMock.Object, _httpContextAccessorMock.Object, _loggerFactoryMock.Object, _cacheServiceMock.Object).Object;
     }
 }
--- tests/CmsEngine.Tests/Services/ServiceTests.cs
+++ tests/CmsEngine.Tests/Services/ServiceTests.cs
@@ -2,7 +2,6 @@
 
 public class ServiceTests : BaseServiceTests
 {
-
     public ServiceTests() : base()
     {
     }
